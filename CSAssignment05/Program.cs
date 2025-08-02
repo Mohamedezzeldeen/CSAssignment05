d66f3752dff1f134acdbaed5863040fdcbcef141
mohamed_ezz <mohamed_ezz@gmail.com>
2025-08-02 21:28:51 +0300

    Q2

--- CSAssignment05/Program.cs
+++ CSAssignment05/Program.cs
@@ -6,14 +6,17 @@
 
         #region Q1.Functions
         //Q1.1By-Value
+        #region Q1.1By-Value
         //public static void ChangeValue(int X , int Y) 
         //{
         //    int Glass = X;
         //    X = Y;
         //    Y = Glass;
         //}
+        #endregion
 
         //Q1.2By-Reference
+        #region Q1.2By-Reference
         //public static void ChangeValue(ref int X, ref int Y)
         //{
         //    int Glass = X;
@@ -21,6 +24,41 @@
         //    Y = Glass;
         //}
         #endregion
+
+        #endregion
+
+        #region Q2.Functions
+
+        //Q2.1By-Value
+        #region Q2.1By-Value
+        //public static int ArrayChange(int[] Arr)
+        //{
+        //    int sum = 0;
+        //    Arr = new int[] { 100, 200, 300 };
+        //    for (int i = 0; i < Arr.Length; i++)
+        //    {
+        //        sum += Arr[i];
+        //    }
+        //    return sum;
+        //}
+        #endregion
+
+        //Q2.2By-Reference
+        #region Q2.2By-Reference
+        //public static int ArrayChange(ref int[] Arr)
+        //{
+        //    int sum = 0;
+        //    Arr = new int[] { 100, 200, 300 };
+        //    for (int i = 0; i < Arr.Length; i++)
+        //    {
+        //        sum += Arr[i];
+        //    }
+        //    return sum;
+        //}
+        #endregion
+
+        #endregion
+
 
         #endregion
 
@@ -56,7 +94,25 @@
 
             #endregion
 
+            #region 2-Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+            //1.Value
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(ArrayChange(Numbers));
+            //Console.WriteLine(Numbers.GetHashCode());
+            //Console.WriteLine(Numbers[0]);
 
+            //Here we find that The Original Array didn't Change
+
+            //2.Reference
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(ArrayChange(ref Numbers));
+            //Console.WriteLine(Numbers.GetHashCode());
+            //Console.WriteLine(Numbers[0]);
+
+            //Here we find that The Original Array Changed
+            #endregion
+
+            
         }
     }
 }
